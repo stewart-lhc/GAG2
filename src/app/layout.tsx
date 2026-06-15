88352fd49317bb964a29f87b6520825069e121f7
Stewart LI <stewart.lhc@qq.com>
2026-06-15 16:42:31 +0800

    chore: add GA4 tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { SiteFooter } from "@/components/SiteFooter";
 import { SiteHeader } from "@/components/SiteHeader";
 import { siteConfig } from "@/data/site";
@@ -21,6 +22,18 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-R25NGR07BJ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R25NGR07BJ');
+          `}
+        </Script>
         <SiteHeader />
         <main>{children}</main>
         <SiteFooter />
